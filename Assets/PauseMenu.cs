e1d467b48bc75f15a737034095587a1279c0fcf5
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-11-24 15:11:34 +0100

    ajout menu pause

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+    public static bool gameIsPaused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameIsPaused)
+            {
+                Resume();
+            }
+            else{
+                Paused();
+            }
+        }
+    }
+    void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPaused = false;
+    }
+}
