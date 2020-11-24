422f018cb3e98a63d40ecbf83631c880aa56fe61
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-11-24 17:10:17 +0100

    sound menu

---
+++ Assets/SoundMenu.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundMenu : MonoBehaviour
+{
+    public Slider mainVolume;
+    public Text mainTxt;
+    public Slider musicVolume;
+    public Text musicTxt;
+    public Slider soundVolume;
+    public Text soundTxt;
+    public Slider voiceVolume;
+    public Text voiceTxt;
+
+    void Update()
+    {
+        mainTxt.text = mainVolume.value.ToString();
+        musicTxt.text = musicVolume.value.ToString();
+        soundTxt.text = soundVolume.value.ToString();
+        voiceTxt.text = voiceVolume.value.ToString();
+    }
+}
