f13bb6f7b1e454759c36df2ba33001e0fa6e3214
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2020-12-21 17:37:17 +0100

    ajout affichage xp
    xp actuel / xp max, c'est temporaire mais ça permet d'y voir plus clair quand on dev

--- Assets/Scripts/XpBar.cs
+++ Assets/Scripts/XpBar.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class XpBar : MonoBehaviour
 {
+    public PlayerXp playerXp;
     public Slider slider;
     public Text txt;
     public int currentLevel;
@@ -10,17 +11,22 @@ public class XpBar : MonoBehaviour
     void Start () 
     {
         currentLevel = 1;
-         txt.text = "Level " + currentLevel;
+        DisplayXp();
      }
 
     public void SetXp(int xp)
     {
         slider.value = xp;
+        DisplayXp();
     }
     public void SetLvl(int lvl, int maxXP)
     {
         currentLevel += lvl;
         slider.maxValue = maxXP;
-        txt.text = "Level " + currentLevel;
+        DisplayXp();
+    }
+    public void DisplayXp()
+    {
+        txt.text = "Level " + currentLevel +"\nCurrentXp: " + playerXp.currentXp + "/" + playerXp.maxXP;
     }
 }
