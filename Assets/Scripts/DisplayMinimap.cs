531cf4fc8b6d16eccf25881b47d5b98e5c3765bf
Netra-Dawkins <netrabisboy@gmail.com>
2021-02-16 16:33:24 +0100

    Minimap

---
+++ Assets/Scripts/DisplayMinimap.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayMinimap : MonoBehaviour
+{
+    public GameObject minimapCanvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minimapCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            minimapCanvas.SetActive(true);
+        }
+        else
+        {
+            minimapCanvas.SetActive(false);
+        }
+
+    }
+}
