d2eecff112c8518993e523e8545fb1a61afc097f
Thomas B3 Dev <47141994+CNQAOSThomas@users.noreply.github.com>
2021-04-08 15:30:46 +0200

    Sauvegarde

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,11 +5,20 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     public float timeOffSet;
-    public Vector3 posOffSet;
+    private Vector3 posOffSet = new Vector3(0f, 0f, -10f);
+    public Vector3 posOffSetGrounded = new Vector3(0f, 1f, -10f); //Pas toucher svp
+    public PlayerMovement movement;
+    
 
     private Vector3 velocity;
     void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffSet, ref velocity, timeOffSet);
+         
+        
+        if (movement.isGrounded)
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffSetGrounded, ref velocity, timeOffSet);
+        } else transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffSet, ref velocity, timeOffSet);
+        
     }
 }
