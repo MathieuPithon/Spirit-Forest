53949025feb598e1aeb7fb6649ec4b7caa84327d
Mathieu Pithon <35268109+Craybloge@users.noreply.github.com>
2021-04-20 15:48:23 +0200

    updtade son

--- Assets/Scripts/SoundMenu.cs
+++ Assets/Scripts/SoundMenu.cs
@@ -13,11 +13,17 @@ public class SoundMenu : MonoBehaviour
     public Slider voiceVolume;
     public Text voiceTxt;
 
+    public GameObject player;
+        
+
     void Update()
     {
         mainTxt.text = mainVolume.value.ToString();
         musicTxt.text = musicVolume.value.ToString();
         soundTxt.text = soundVolume.value.ToString();
         voiceTxt.text = voiceVolume.value.ToString();
+    }
+    public void SetVolume(float Volume){
+        // player.GetComponent<AudioSource>
     }
 }
