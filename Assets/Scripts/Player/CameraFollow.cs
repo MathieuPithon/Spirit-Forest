44202265af52d064b2da7febe5eaad7f412d9e1f
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2021-12-20 11:27:51 +0100

    rangement

--- Assets/Scripts/Player/CameraFollow.cs
+++ Assets/Scripts/Player/CameraFollow.cs
@@ -5,20 +5,21 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     public float timeOffSet;
-    private Vector3 posOffSet = new Vector3(0f, 0f, -10f);
-    public Vector3 posOffSetGrounded = new Vector3(0f, 1f, -10f); //Pas toucher svp
+    private Vector3 posOffSet = new Vector3(0f, 2f, -10f); // ??? le Y c'est posOffSetGrounded -1 (la variable d'en dessous)
+    public Vector3 posOffSetGrounded;
     public PlayerMovement movement;
-    
+
 
     private Vector3 velocity;
     void Update()
     {
-         
-        
+
+
         if (movement.isGrounded)
         {
             transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffSetGrounded, ref velocity, timeOffSet);
-        } else transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffSet, ref velocity, timeOffSet);
-        
+        }
+        else transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffSet, ref velocity, timeOffSet);
+
     }
 }
