076c3e7e77dedf5b2e4fe27c2eea0efd84a06190
dgrammont <dylanlaudat@orange.fr>
2022-01-26 09:19:54 +0100

    Modification du dash

--- Assets/Scripts/Player/Player_Dash.cs
+++ Assets/Scripts/Player/Player_Dash.cs
@@ -7,6 +7,9 @@ public class Player_Dash : MonoBehaviour
     public float dashLenght;
     public float dashSpeed;
     public Rigidbody2D rb;
+
+     public GameObject theplayer;
+     public PlayerCombat combat;
     void Start()
     {
 
@@ -28,10 +31,12 @@ public class Player_Dash : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftShift) && transform.localScale.x > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x + dashLenght, rb.velocity.y);
+            theplayer.GetComponent<Animator>().Play("PlayerDash");
         }
         else if (Input.GetKeyDown(KeyCode.LeftShift) && transform.localScale.x < 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x - dashLenght, rb.velocity.y);
+            theplayer.GetComponent<Animator>().Play("PlayerDash");
         }
     }
 
