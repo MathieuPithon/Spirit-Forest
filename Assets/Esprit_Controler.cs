0c6a45f24f20269a8c83ebe37c8df580bb8c183c
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-01-31 15:39:43 +0100

    nouveau player

---
+++ Assets/Esprit_Controler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Cyriaque - Fait tous les if est lance les differents scripts
+
+public class Esprit_Controler : MonoBehaviour
+{
+    public Esprit_Mouvement Esprit_Mouvement;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Esprit_Mouvement.EspritMouvement();
+
+    }
+}
---
+++ Assets/Scripts/Esprit/Esprit_Mouvement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Cyriaque - Gere les mouvement droite / gauche de l'esprit
+
+public class Esprit_Mouvement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    private float horizontalMovement;
+    private Vector3 velocity = Vector3.zero;
+
+
+    public void EspritMouvement()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * moveSpeed;
+        Vector3 wantedVelocity = new Vector2(horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, wantedVelocity, ref velocity, 0.5f);
+    }
+}
