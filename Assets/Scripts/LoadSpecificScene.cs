e6f26a7bf4fce844a579dbddfe9ee5244dac67ea
Jonas701 <77495120+Jonas701@users.noreply.github.com>
2022-02-11 19:04:10 +0100

    transitions

--- Assets/Scripts/LoadSpecificScene.cs
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -6,6 +6,11 @@ public class LoadSpecificScene : MonoBehaviour
 {
     public string sceneName;
     public Animator fadeSystem;
+
+    private void Awake()
+    {
+        fadeSystem = GameObject.FindGameObjectWithTag("FadeSystem").GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
@@ -17,19 +22,24 @@ public class LoadSpecificScene : MonoBehaviour
 
     public IEnumerator LoadNextScene()
     {
-        fadeSystem.SetTrigger("Start");
+        fadeSystem.SetTrigger("FadeIn");
+       /**
         PlayerMovement.instance.enabled = false;
         GameObject.Find("Player").GetComponent<PlayerActions>().enabled = false;
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
         GameObject.Find("Player").GetComponent<PlayerCombat>().enabled = false;
         PlayerMovement.instance.rb.velocity = Vector2.zero;
         PlayerMovement.instance.animator.SetFloat("Speed", 0);
+        **/
 
         yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
+        /**
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         PlayerMovement.instance.enabled = true;
         GameObject.Find("Player").GetComponent<PlayerActions>().enabled = true;
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
         GameObject.Find("Player").GetComponent<PlayerCombat>().enabled = true;
+        **/
     }
 }
