3bb2ccdb18f1b17c834c3d76b0ea7a19e2be259f
Jonas701 <77495120+Jonas701@users.noreply.github.com>
2022-02-13 08:00:11 +0100

    Scène 2 + bug crossfade scène forêt

--- Assets/Scripts/LoadSpecificScene.cs
+++ Assets/Scripts/LoadSpecificScene.cs
@@ -22,24 +22,23 @@ public class LoadSpecificScene : MonoBehaviour
 
     public IEnumerator LoadNextScene()
     {
-        fadeSystem.SetTrigger("Start");
-       /**
+        fadeSystem.SetTrigger("FadeIn"); /**
         PlayerMovement.instance.enabled = false;
         GameObject.Find("Player").GetComponent<PlayerActions>().enabled = false;
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = false;
         GameObject.Find("Player").GetComponent<PlayerCombat>().enabled = false;
         PlayerMovement.instance.rb.velocity = Vector2.zero;
         PlayerMovement.instance.animator.SetFloat("Speed", 0);
-        **/
 
-        yield return new WaitForSeconds(1f);
-        SceneManager.LoadScene(sceneName);
-        /**
+       
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         PlayerMovement.instance.enabled = true;
         GameObject.Find("Player").GetComponent<PlayerActions>().enabled = true;
         GameObject.Find("Player").GetComponent<PlayerMovement>().enabled = true;
         GameObject.Find("Player").GetComponent<PlayerCombat>().enabled = true;
         **/
+         yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(sceneName);
     }
 }
