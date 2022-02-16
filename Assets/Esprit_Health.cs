118fab9e4edc03bda453ab8dbb119a115a1a74f2
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-02-16 15:42:53 +0100

    invisibilité apres prise de dmg

--- Assets/Esprit_Health.cs
+++ Assets/Esprit_Health.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Cyriaque - dans 'public Healthbar' il faut mettre la health bar qui est dans canvas 
-// si le joueur rentre en collision avec qql chose du layer Enemy alors il prend des dmg (fonction TakeDamage)
-//https://www.youtube.com/watch?v=BLfNP4Sc_iA
+// si le joueur rentre en collision avec qql chose du layer Enemy alors 
+// colide = true et il prend des dmgs , quand il arrete de toucher un ennemi colide = false 
+// quand il se fait toucher une coroutine de 2 s start et pendant ce temps il est invinsible 
 public class Esprit_Health : MonoBehaviour
 {
     private int maxHealth = 100;
     private int currentHealth;
     public HealthBar healthBar;
+    private bool invicibility = false;
+    private bool colide = false;
 
     void Start()
     {
@@ -17,20 +20,46 @@ public class Esprit_Health : MonoBehaviour
         healthBar.SetMaxHealth(maxHealth);
     }
 
-    
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        if (invicibility == false)
         {
-            TakeDamage(20);
+            if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+            {
+                colide = true;
+            }
+
         }
     }
 
+    void onCollisionExit2D(Collision2D collision)
+    {
+        colide = false;
+    }
+
+    void Update()
+    {
+        if (colide && !invicibility)
+        {
+            TakeDamage(20);
+            StartCoroutine(Delay());
+        }
+    }
 
     void TakeDamage(int damage)
     {
         currentHealth -= damage;
         healthBar.SetHealth(currentHealth);
     }
+
+    IEnumerator Delay()
+    {
+        invicibility = true;
+        Debug.Log("true");
+        yield return new WaitForSeconds(2f);
+        invicibility = false;
+    }
+
 
 }
