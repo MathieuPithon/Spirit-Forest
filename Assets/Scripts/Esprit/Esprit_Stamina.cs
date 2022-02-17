40f1732e08c224f34743c40e6e157e27f458605c
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-02-17 15:07:33 +0100

    stamina sans la regene

---
+++ Assets/Scripts/Esprit/Esprit_Stamina.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Esprit_Stamina : MonoBehaviour
+{
+    private int maxStamina = 100;
+    private int currentStamina;
+    public StaminaBar StaminaBar;
+    private bool cd = false;
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        StaminaBar.SetMaxStamina(maxStamina);
+    }
+
+
+    void Update()
+    {
+        if (!cd)
+        {
+            RegenStamina();
+        }
+    }
+
+    void ReduceStamina(int amount)
+    {
+        currentStamina -= amount;
+        StaminaBar.SetStamina(currentStamina);
+        Delay();
+    }
+
+    void RegenStamina() {
+        
+    }
+
+    IEnumerator Delay()
+    {
+        cd = true;
+        yield return new WaitForSeconds(3f);
+        cd = false;
+    }
+
+
+}
