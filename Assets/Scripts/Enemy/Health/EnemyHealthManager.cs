945270fd071a15c0999f5d37ae25a6b0b763296e
dgrammont <dylanlaudat@orange.fr>
2022-02-21 16:08:50 +0100

    Update EnemyHealthManager.cs

--- Assets/Scripts/Enemy/Health/EnemyHealthManager.cs
+++ Assets/Scripts/Enemy/Health/EnemyHealthManager.cs
@@ -29,7 +29,7 @@ public class EnemyHealthManager : MonoBehaviour
             deathSound.Play();            
             //jouer animation de mort
             Samourai.GetComponent<Animator>().Play("SamouraiDie");
-            Destroy(gameObject);
+            Destroy(Samourai);
         }
         
     }    
