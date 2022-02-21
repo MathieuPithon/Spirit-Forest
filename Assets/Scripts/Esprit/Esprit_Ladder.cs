8c6e0268e9cc0fc5d01add7988a8ecf33a90f565
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-02-21 11:43:24 +0100

    a jour

---
+++ Assets/Scripts/Esprit/Esprit_Ladder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esprit_Ladder : MonoBehaviour
+{
+    //     private float verticalMovement;
+    //     public Rigidbody2D rb;
+    //     private Vector3 velocity = Vector3.zero;
+    //     public float moveSpeed;
+    //     void ClimbLadder()
+    //     {
+
+    //         verticalMovement = Input.GetAxis("Vertical") * Time.fixedDeltaTime * moveSpeed;
+    //         Vector3 wantedVelocity = new Vector2(verticalMovement, rb.velocity.x);
+    //         rb.velocity = Vector3.SmoothDamp(rb.velocity, wantedVelocity, ref velocity, .05f);
+    //     }
+    //     void Update()
+    //     {
+    //         if (Input.GetKeyDown("z"))
+    //         {
+    //             Debug.Log("brrbrbrbbr");
+    //             ClimbLadder();
+    //         }
+
+    //     }
+}
