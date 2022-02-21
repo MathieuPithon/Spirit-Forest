232ee940a6aa837dfc625b121412b660efb067cc
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-02-21 10:18:11 +0100

    Espirt_IsGrounded

---
+++ Assets/Scripts/Esprit/Esprit_IsGrounded.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esprit_IsGrounded : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isGrounded;
+    public Transform groundCheck;
+    private float groundCheckRadius = 0.63f;
+    public LayerMask collisionGroundLayers;
+
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionGroundLayers);
+    }
+
+}
