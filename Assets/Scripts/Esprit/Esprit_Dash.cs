2113c8efb8460ee90d9fdad6d4edf431276367b0
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-02-22 16:20:06 +0100

    dash + ladder

--- Assets/Scripts/Esprit/Esprit_Dash.cs
+++ Assets/Scripts/Esprit/Esprit_Dash.cs
@@ -12,36 +12,39 @@ public class Esprit_Dash : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     public Esprit_Stamina Stamina;
     public GameObject esprit;
+    private bool cd = false;
 
     void Update()
     {
-        //désactive la gravité pendant le dash
-        if (rb.velocity.x > 10)
+        if (!cd)
         {
-            rb.GetComponent<Rigidbody2D>().gravityScale = 0f;
-
-        }
-        else
-        {
-            rb.GetComponent<Rigidbody2D>().gravityScale = 2f;
-
-        }
-
-        // transform.localScale.x > 0f true si le perso regarde a droite 
-        if (Input.GetKeyDown(KeyCode.LeftShift) && transform.localScale.x > 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x + dashLenght, rb.velocity.y);
-            Stamina.ReduceStamina(10);
-            esprit.GetComponent<Animator>().Play("PlayerDash");
-        }
-        // transform.localScale.x > 0f true si le perso regarde a gauche 
-        else if (Input.GetKeyDown(KeyCode.LeftShift) && transform.localScale.x < 0f)
-        {
-            rb.velocity = new Vector2(rb.velocity.x - dashLenght, rb.velocity.y);
-            Stamina.ReduceStamina(10);
-            esprit.GetComponent<Animator>().Play("PlayerDash");
+            // transform.localScale.x > 0f true si le perso regarde a droite 
+            if (Input.GetKeyDown(KeyCode.LeftShift) && transform.localScale.x > 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x + dashLenght, rb.velocity.y);
+                Stamina.ReduceStamina(10);
+                esprit.GetComponent<Animator>().Play("PlayerDash");
+                StartCoroutine(DelayCd());
+            }
+            // transform.localScale.x > 0f true si le perso regarde a gauche 
+            else if (Input.GetKeyDown(KeyCode.LeftShift) && transform.localScale.x < 0f)
+            {
+                rb.velocity = new Vector2(rb.velocity.x - dashLenght, rb.velocity.y);
+                Stamina.ReduceStamina(10);
+                esprit.GetComponent<Animator>().Play("PlayerDash");
+                StartCoroutine(DelayCd());
+            }
         }
     }
 
+    IEnumerator DelayCd()
+    {
+        cd = true;
+        rb.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+        yield return new WaitForSeconds(0.15f);
+        rb.constraints &= ~RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezeRotation;
+        yield return new WaitForSeconds(3f);
+        cd = false;
+    }
 
 }
--- Assets/Scripts/Esprit/Esprit_Ladder.cs
+++ Assets/Scripts/Esprit/Esprit_Ladder.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class Esprit_Ladder : MonoBehaviour
 {
-    //     private float verticalMovement;
-    //     public Rigidbody2D rb;
-    //     private Vector3 velocity = Vector3.zero;
-    //     public float moveSpeed;
-    //     void ClimbLadder()
-    //     {
+    private float verticalMovement;
+    public Rigidbody2D rb;
+    private Vector3 velocity = Vector3.zero;
+    public float moveSpeed;
 
-    //         verticalMovement = Input.GetAxis("Vertical") * Time.fixedDeltaTime * moveSpeed;
-    //         Vector3 wantedVelocity = new Vector2(verticalMovement, rb.velocity.x);
-    //         rb.velocity = Vector3.SmoothDamp(rb.velocity, wantedVelocity, ref velocity, .05f);
-    //     }
-    //     void Update()
-    //     {
-    //         if (Input.GetKeyDown("z"))
-    //         {
-    //             Debug.Log("brrbrbrbbr");
-    //             ClimbLadder();
-    //         }
 
-    //     }
+
+
+    void ClimbLadder()
+    {
+        rb.gravityScale = 0.0f;
+        verticalMovement = Time.fixedDeltaTime * moveSpeed;
+        Vector3 wantedVelocity = new Vector2(rb.velocity.x, verticalMovement);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, wantedVelocity, ref velocity, .05f);
+    }
+    void Update()
+    {
+        if (Input.GetKey("z"))
+        {
+            ClimbLadder();
+        }
+        if (Input.GetKeyUp("z"))
+        {
+            rb.velocity = Vector3.zero;
+        }
+
+    }
 }
