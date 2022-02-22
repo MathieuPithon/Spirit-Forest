3898e0dab149f5b183bc85900c5d0d314eb5220b
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-02-22 11:18:26 +0100

    Esprit modif combat

--- Assets/Scripts/Esprit/Esprit_Combat.cs
+++ Assets/Scripts/Esprit/Esprit_Combat.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//Cyriaque - attaque lourde / attaque legere c'est le bordel demandé irl si pb
 public class Esprit_Combat : MonoBehaviour
 {
     [HideInInspector]
@@ -10,20 +12,32 @@ public class Esprit_Combat : MonoBehaviour
     public float attackRange;
     public LayerMask enemies;
     private float startTime;
+    public Animator anim;
+    private bool letGo = true;
+    private bool cantAttack;
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.Mouse0))
+        if (Input.GetKeyUp(KeyCode.Mouse0) && cantAttack == false)
         {
             Combat(20);
+            StartCoroutine(Delay(0.25f));
+            anim.SetTrigger("Attack");
         }
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             startTime = Time.time;
         }
-        if (Input.GetKey(KeyCode.Mouse0) && Time.time - startTime > 1f)
+        if (Input.GetKey(KeyCode.Mouse0) && Time.time - startTime > 1f && letGo && cantAttack == false)
         {
             Combat(40);
+            StartCoroutine(Delay(2f));
+            anim.SetTrigger("Attack");
             startTime = Time.time;
+            letGo = false;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            letGo = true;
         }
     }
 
@@ -35,6 +49,13 @@ public class Esprit_Combat : MonoBehaviour
         {
             enemiesToDamage[i].GetComponent<EnemyHealthManager>().HurtEnemy(dmg);
         }
+    }
+
+    IEnumerator Delay(float time)
+    {
+        cantAttack = true;
+        yield return new WaitForSeconds(time);
+        cantAttack = false;
     }
     void OnDrawGizmosSelected()
     {
