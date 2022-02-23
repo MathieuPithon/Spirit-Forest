e4a090a90db63ffaca2a1c1bdd6bd8da028296f5
dgrammont <dylanlaudat@orange.fr>
2022-02-23 14:59:48 +0100

    spawner

--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -6,21 +6,37 @@ public class Spawner : MonoBehaviour
 {
     public Transform[] spawnPoints;
     public GameObject[] enemyPrefabs;
-    
+    private bool play;
+
     void Start()
     {
-        
+        StartCoroutine(myWaitCoroutine());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
-        {
-            int randEnemy = Random.Range(0, enemyPrefabs.Length);
-            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
 
-            Instantiate(enemyPrefabs[0],spawnPoints[randSpawnPoint].position, transform.rotation);
-        }    
+        if(!play){
+            StartCoroutine(myWaitCoroutine());
+        }
+
+        /*if(Input.GetMouseButtonDown(0))
+        {
+            
+        }    */
     }
+    IEnumerator myWaitCoroutine()
+    {
+        
+        play= true;
+        yield return new WaitForSeconds(0.8f);// Wait for one second
+
+        int randEnemy = Random.Range(0, enemyPrefabs.Length);
+        int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+        Instantiate(enemyPrefabs[0], spawnPoints[randSpawnPoint].position, transform.rotation);
+        play=false;
+    }
+
 }
+
