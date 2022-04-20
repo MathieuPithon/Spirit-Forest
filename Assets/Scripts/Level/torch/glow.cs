feee7451151d63669fa14ade327172d7ea5b5458
dgrammont <dylanlaudat@orange.fr>
2022-04-20 11:26:23 +0200

    Light

---
+++ Assets/Scripts/Level/torch/glow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class glow : MonoBehaviour
+{
+    Renderer rend;
+    float colInt;
+    Color c;
+    public float minColInt = 0.5f, maxCalInt = 1f;
+    // Start is called before the first frame update
+    void Start(){
+        rend = GetComponent<SpriteRenderer> ();
+    }
+    // Update is called once per frame
+    void Update(){
+        colInt = Random.Range (minColInt, maxCalInt);
+        c = rend.material.color;
+        c.a = colInt;
+        rend.material.color = c;
+    }
+}
