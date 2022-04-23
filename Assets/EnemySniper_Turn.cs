23df2e78e845c78c4cd8ed56ce117fbdc4ab5877
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-04-23 14:16:38 +0200

    enemysniper

---
+++ Assets/EnemySniper_Turn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySniper_Turn : MonoBehaviour
+{
+
+    public bool needTurn;
+    void OnTriggerEnter2D(Collider2D Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            needTurn = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            needTurn = false;
+        }
+    }
+}
