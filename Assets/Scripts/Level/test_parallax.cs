2d5cdaa8e39110b08b75868a2d9f1c6a1034ffba
Jonas701 <77495120+Jonas701@users.noreply.github.com>
2022-04-23 11:30:41 +0200

    test parallax
    pour débug le back ground qui devient noir

---
+++ Assets/Scripts/Level/test_parallax.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_parallax : MonoBehaviour {
+
+    private float lenght, startpos;
+    public GameObject cam;
+    public float parallaxEffect;
+
+    void Start(){
+        startpos = transform.position.x;
+        lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+
+    }
+
+    void Update(){
+        float temp =(cam.transform.position.x * (1-parallaxEffect));
+        float dist = (cam.transform.position.x * parallaxEffect);
+
+        transform.position = new Vector3(startpos + dist,transform.position.y, transform.position.z);
+
+        if(temp>startpos + lenght) startpos += lenght;
+        else if (temp<startpos - lenght) startpos-=lenght;
+
+    }
+
+
+}
+
+
