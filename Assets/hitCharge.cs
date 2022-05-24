2399242f089a40e981a321cda5045d9a562eb38b
CyriaqueSoden <73218277+CyriaqueSoden@users.noreply.github.com>
2022-05-24 15:30:08 +0200

    rezuq

---
+++ Assets/hitCharge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitCharge : MonoBehaviour
+{
+    public bool hited = false;
+
+    void OnTriggerEnter2D(Collider2D Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            hited = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D Player)
+    {
+        if (Player.gameObject.tag == "Player")
+        {
+            hited = false;
+        }
+    }
+}
